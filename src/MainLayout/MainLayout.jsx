31b729091c2fed19e21763951dda815dea8d1e9f
Md Azad Hossain <sokalazad@gmail.com>
2024-11-01 13:43:31 +0100

    navbar and header are added

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer/Footer";
+import Navbar from "../components/Navbar/Navbar";
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h1>Main Content will be here.</h1>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default MainLayout;
